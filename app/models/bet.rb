5c31bd337b3579fced073af710dcaa882dbdba74
Tommy Lees <thomas.lees112@gmail.com>
2017-08-28 16:02:56 +0100

    changed bet model

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -2,9 +2,6 @@ class Bet < ApplicationRecord
   belongs_to :user
   belongs_to :odd
   has_one :match, through: :odd
-  has_one :home_team, through: :match ## HOW TO WRITE THIS
-  has_one :away_team, through: :match ## HOW TO WRITE THIS
-  # eg has_many :owned_odds, through: :bookings, source: :odds
 
   # VALIDATIONS
   validates :user_id, uniqueness: { scope: :odd_id }
