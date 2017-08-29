37961a174dce6a3638a3fb1d79954ee064b58496
Tommy Lees <thomas.lees112@gmail.com>
2017-08-29 13:17:06 +0100

    implmenent league and other methods

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,11 +1,12 @@
 class Match < ApplicationRecord
   # ASSOCIATIONS
-  belongs_to :home_team
-  belongs_to :away_team
+  belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
+  belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
   belongs_to :league
   has_many :odds
   has_many :bets, through: :odds
   has_many :users, through: :bets ##HOW TO WRITE THIS?
+  ## ADDED
 
   # VALIDATIONS
   validates :home_team, presence: true
@@ -13,4 +14,12 @@ class Match < ApplicationRecord
   validates :match_date, presence: true
   validates :gameweek, presence: true, numericality: true
   validates :league, presence: true
+
+  def teams
+    { home: home_team, away: away_team }
+  end
+
+  def flat_teams
+    [ home_team, away_team ]
+  end
 end
