4d1a86c693467332962b6b53551582f13a062e0f
Tommy Lees <thomas.lees112@gmail.com>
2017-08-29 17:51:57 +0100

    added migration references from teams > team_model_outputs

---
+++ db/migrate/20170829164514_add_model_columns_to_matches.rb
@@ -0,0 +1,12 @@
+class AddModelColumnsToMatches < ActiveRecord::Migration[5.0]
+  def change
+    add_column :matches, :home_adj_goals, :float
+    add_column :matches, :away_adj_goals, :float
+    add_column :matches, :home_shot_xg, :float
+    add_column :matches, :away_shot_xg, :float
+    add_column :matches, :home_non_shot_xg, :float
+    add_column :matches, :away_non_shot_xg, :float
+    add_column :matches, :home_adj_goals, :float
+    add_column :matches, :away_adj_goals, :float
+  end
+end
