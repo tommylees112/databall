1e2fd819b27a000f8a447dc39cb49e838d534e13
Adrian Mustea <mustea.adrian95@gmail.com>
2017-08-31 15:21:09 +0100

    fixed the redirection path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
+   authenticated :user do
+    root to: 'odds#index'
+  end
+
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "registrations", sessions: "sessions"}
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :odds, only: [:show, :index] do
@@ -10,5 +14,7 @@ Rails.application.routes.draw do
 
 
   get "/dashboard", to: "users#dashboard"
+
+
 
 end
