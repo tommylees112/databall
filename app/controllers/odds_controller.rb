4c4ce0d59e367b9f2fe36b756fb0741864c0e7fc
Hamish Hall <hachall@hotmail.com>
2017-09-04 15:58:24 +0100

    front page tabs, cards simplified, fractional and decimal implemented

--- app/controllers/odds_controller.rb
+++ app/controllers/odds_controller.rb
@@ -1,7 +1,42 @@
 class OddsController < ApplicationController
 
   def index
-    @odds = Odd.where(odds_bias_filter: true).order(rating: :DESC)
+    if params[:league].present?
+      query = <<-SQL
+        SELECT *
+        FROM odds o1
+        JOIN matches ON o1.match_id = matches.id
+        JOIN leagues ON matches.league_id = leagues.id
+        WHERE leagues.name = :league_name
+        AND o1.id IN (
+          SELECT id
+            FROM odds o2
+            WHERE o2.match_id = o1.match_id
+            AND o2.odds_bias_filter = true
+          ORDER BY o2.rating DESC
+          LIMIT 1
+        )
+        ORDER BY o1.rating DESC
+      SQL
+      @odds = Odd.find_by_sql [ query, league_name: params[:league] ]
+    else
+      # Recommended:
+      @odds = Odd.find_by_sql <<-SQL
+        SELECT *
+        FROM odds o1
+        WHERE o1.id IN (
+          SELECT id
+            FROM odds o2
+            WHERE o2.match_id = o1.match_id
+            AND o2.odds_bias_filter = true
+          ORDER BY o2.rating DESC
+          LIMIT 1
+        )
+        ORDER BY o1.rating DESC
+        LIMIT 5
+      SQL
+    end
+
     @bet = Bet.new
   end
 
