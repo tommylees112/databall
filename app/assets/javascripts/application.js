ac23424cc2e82ba8d3e63b9da47a7a814c36c9c4
Tommy Lees <thomas.lees112@gmail.com>
2017-09-05 14:53:27 +0100

    loadsa shit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,4 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require Chart.min
-//= require_tree .
-
-// <--> TOMMYS CODE <-->
-// require Chart.bundle
-//= require chartkick
-// require highcharts
-// require turbolinks
+//= require_tree ./app
