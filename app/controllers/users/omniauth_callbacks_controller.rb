81c798b0b6e490a24ae0f7dfccebbef60a7542b3
Tommy Lees <thomas.lees112@gmail.com>
2017-09-06 21:50:56 +0100

    uncomment stripe payment lines

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,17 +6,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
 
-    # user.stripe_customer = Stripe::Customer.create( email: user.email )
+    user.stripe_customer = Stripe::Customer.create( email: user.email )
 
-    # user.stripe_subscription = Stripe::Subscription.create(
-    #   customer: user.stripe_customer['id'],
-    #   items: [
-    #     {
-    #       plan: 'basic-plan-databall',
-    #     },
-    #   ],
-    #   trial_end: (Time.now + 30.days).to_i,
-    # )
+    user.stripe_subscription = Stripe::Subscription.create(
+      customer: user.stripe_customer['id'],
+      items: [
+        {
+          plan: 'basic-plan-databall',
+        },
+      ],
+      trial_end: (Time.now + 30.days).to_i,
+    )
 
     user.save
 
