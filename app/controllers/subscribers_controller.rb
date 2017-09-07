9ec4c22f7b20b1749d153843cb896ec358984375
Adrian Mustea <mustea.adrian95@gmail.com>
2017-09-07 18:22:31 +0100

    works fine now,another payment sht

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,4 +1,5 @@
 class SubscribersController < ApplicationController
+
   def cancel_plan
     subscription = Stripe::Subscription.retrieve("sub_BM93lo0kON8IxI")
     subscription.delete(:at_period_end => true)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905163600) do
+ActiveRecord::Schema.define(version: 20170907171719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,7 +165,7 @@ ActiveRecord::Schema.define(version: 20170905163600) do
     t.datetime "token_expiry"
     t.boolean  "admin",                  default: false, null: false
     t.jsonb    "stripe_customer"
-    t.boolean  "access",                 default: true
+    t.boolean  "access",                 default: false
     t.jsonb    "stripe_subscription"
     t.datetime "trial_end"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
